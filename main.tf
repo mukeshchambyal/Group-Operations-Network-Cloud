2c34209bbc46a0766a712f0f1b322846f7d1b5b6
Mukesh.Kumar <mukesh.kumar@axa.com>
2026-05-12 11:20:57 +0800

    config fix

--- main.tf
+++ main.tf
@@ -7,42 +7,25 @@ terraform {
   }
   required_providers {
     aws = {
-      version = "= 5.80.0"
       source  = "hashicorp/aws"
-    }  
-  }   
+      version = "= 5.80.0"
+    }
+  }
 }
+
 provider "aws" {
-    region              = "sa-east-1"
+  region = "sa-east-1"
+  default_tags {
+    tags = {
+      "global.opco" = "Personal"
+    }
+  }
 }
+
 # 1. VPC Definition
 resource "aws_vpc" "vpn" {
   cidr_block = "100.78.33.0/24"
   tags = {
     Name = "Group-Cloud-VPC"
-  }
-}
-
-terraform {
-  cloud {
-    organization = "Group-Operations-Network-Cloud"
-    workspaces {
-      name = "Group-Operations-Network-Cloud"
-    }
-  }
-  required_providers {
-    aws = {
-      version = "= 5.80.0"
-      source  = "hashicorp/aws"
-    }  
-  }   
-}
-
-provider "aws" {
-    region              = "sa-east-1"
-    default_tags {
-        tags = {
-            "global.opco"         = "Personal"
-        }
   }
 }
